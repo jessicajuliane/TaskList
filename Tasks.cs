a36318cc23cf8ba1ba49f268d70a5bf1dd90a136
jessicajuliane <jessica.juliane@outlook.com.br>
2019-09-12 11:24:22 -0300

    Ajustar campos obrigatorios
    Campos Status e Títulos são obrigatórios na Task

--- Tasks.cs
+++ Tasks.cs
@@ -5,8 +5,10 @@ namespace TaskList.Models
 {
     public class Tasks
     {
+        [Required]
         private bool status { get; set; }
         private int taskId { get; set; }
+        [Required]
         private string titulo { get; set; }
         private string descricao { get; set; }
     }
